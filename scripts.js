67a5a91af4e308f9e273c879622b8cbddf42d0cf
leonardo <leonardocave934@gmail.comm>
2021-04-24 14:18:59 -0300

    trabalho

--- scripts.js
+++ scripts.js
@@ -1,33 +1,29 @@
 
-//isso aqui pega o botão "somar"
+
 let btsomar = document.getElementById("btsomar")
 
-// isso aqui pega a ação do botão 
+
 btsomar.addEventListener("click", function () {
 
-    //----------------------------------
-    //isso aqui pega o valor do input 1
     let valor1Soma = document.getElementById("valor1Soma")
     valor1Soma = valor1Soma.value
-    //----------------------------------
 
-    //----------------------------------
-    //isso aqui pega o valor do input 1
-    let valor2SomaSoma = document.getElementById("valor2Soma")
+    let valor2Soma = document.getElementById("valor2Soma")
     valor2Soma = valor2Soma.value
-    //----------------------------------
+    if (valor1Soma < 0) {
+        alert("Não pode colocar Número negativo")
+    } else {
+        let soma = Number(valor1Soma) + Number(valor2Soma)
+        resultadoSoma = document.getElementById("resultadoSoma")
+        resultadoSoma.innerHTML = soma
+    }
+    if (valor1Soma == 0) {
+        alert("Não pode ser dividido por 0")
+    } else {
 
-    //----------------------------------
-    // isso aqui faz a soma
-    let soma = Number(valor1Soma) + Number(valor2Soma)
-    //----------------------------------
+    }
 
 
-    //----------------------------------
-    // isso aqui faz aparecer depois do igual(=) na tela
-    let resultadoSoma = document.getElementById("resultadoSoma")
-    resultadoSoma.innerHTML = soma
-    //----------------------------------
 })
 
 let btSub = document.getElementById("btSub")
@@ -39,10 +35,20 @@ btSub.addEventListener("click", function () {
     let valor2Sub = document.getElementById("valor2Sub")
     valor2Sub = valor2Sub.value
 
-    let Sub = Number(valor1Sub) - Number(valor2Sub)
 
-    let resultadoSub = document.getElementById("resultadoSub")
-    resultadoSub.innerHTML = Sub
+    if (valor1Sub < 0) {
+        alert("Não pode colocar Número negativo")
+    } else {
+        let Sub = Number(valor1Sub) - Number(valor2Sub)
+
+        let resultadoSub = document.getElementById("resultadoSub")
+        resultadoSub.innerHTML = Sub
+    }
+
+    if (valor1Sub == 0) {
+        alert("Não pode ser dividido por 0")
+    }
+
 
 })
 
@@ -56,34 +62,39 @@ btMul.addEventListener("click", function () {
     let valor2Mul = document.getElementById("valor2Mul")
     valor2Mul = valor2Mul.value
 
-    let Mul = Number(valor1Mul) * Number(valor2Mul)
+    if (valor1Mul < 0) {
 
-    let resultadoMul = document.getElementById("resultadoMul")
-    resultadoMul.innerHTML = Mul
-    
+        alert("Não pode colocar Número negativo")
+    } else {
+        let Mul = Number(valor1Mul) * Number(valor2Mul)
+
+        let resultadoMul = document.getElementById("resultadoMul")
+        resultadoMul.innerHTML = Mul
+
+    }
 })
-
 let btDiv = document.getElementById("btDiv")
 
 btDiv.addEventListener("click", function () {
-
     let valor1Div = document.getElementById("valor1Div")
     valor1Div = valor1Div.value
 
     let valor2Div = document.getElementById("valor2Div")
     valor2Div = valor2Div.value
 
-    let Div = Number(valor1Div) / Number(valor2Div)
 
-    let resultadoDiv = document.getElementById("resultadoDiv")
-    resultadoDiv.innerHTML = Div
+    if (valor1Div < 0) {
+        alert("Não pode colocar Número negativo")
+    } else {
+        let Div = Number(valor1Div) - Number(valor2Div)
+
+        let resultadoDiv = document.getElementById("resultadoDiv")
+        resultadoDiv.innerHTML = Div
+    }
+
+    if (valor1Div == 0) {
+        alert("Não pode ser dividido por 0")
+    }
+
 
 })
-
-
-
-
-
-
-
-
